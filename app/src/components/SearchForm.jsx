085e5c7e564b57db5a7ba0cca70bc6ffe6464d35
IVB107 <ianvb107@gmail.com>
2019-03-13 12:27:11 -0500

    Refactored SearchContainer and TracksContainer components
    Co-authored-by: Steve <41658450+heyjuststart@users.noreply.github.com>

--- app/src/components/SearchForm.jsx
+++ app/src/components/SearchForm.jsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { searchingTracks } from '../actions';
+import { debounce } from 'underscore';
+// import { WSAEDQUOT } from 'constants';
 
-const SearchForm = (props) => {
-  return ;
-};
 
-export default SearchForm;
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: ''
+    };
+  }
+
+  componentDidMount() {
+    this.props.searchingTracks('');
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    this.searchingTracksDebounced(e.target.value);
+  };
+
+  searchingTracksDebounced = debounce(value => {
+    this.props.searchingTracks(value);
+  }, 500);
+
+  render() {
+    return (
+      <div>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            this.props.searchingTracks(this.state.searchTerm);
+          }}
+        >
+          <input
+            type="text"
+            name="searchTerm"
+            placeholder="Search Track"
+            value={this.state.searchTerm}
+            onChange={this.onChange}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  tracks: state.tracks.results,
+  loading: state.tracks.searching
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    searchingTracks
+  }
+)(Search);
---
+++ app/src/components/TracksContainer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Loader } from './withLoading';
+import Track from './Track';
+import { connect } from 'react-redux';
+
+const ResultsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 300px;
+    margin: 0 auto;
+`;
+
+const TracksContainer = props => {
+    return (
+        <ResultsContainer>
+            <Loader isLoading={props.loading}>
+            {props.tracks.map(track => (
+                <Track key={track.track_id} track={track} />
+            ))}
+            </Loader>
+        </ResultsContainer>
+    )
+}
+
+const mstp = state => ({
+    loading: state.tracks.searching,
+    tracks: state.tracks.results
+    
+})
+
+export default connect(mstp)(TracksContainer);
